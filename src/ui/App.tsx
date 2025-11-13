9c64b91ab3ed0d3668fe46fcbec0d10e9718497f
Dihyah Adib <dihyah.adib@gmail.com>
2025-11-13 00:11:12 -0500

    frame count

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -4,6 +4,7 @@ import "./App.css";
 function App() {
   const [imgPath, setImgPath] = useState<string | null>(null);
   const [filePath, setFilePath] = useState<string | null>(null);
+  const [frames, setFrameCount] = useState<number | undefined>(1);
 
   async function handleOpen() {
     const newFilePath = await window.electronAPI.openImageDialog();
@@ -24,29 +25,51 @@ function App() {
   }
 
   return (
-    <div style={{ padding: 20 }}>
-      <h3>
-        {filePath ? (
-          <>
-            Img path selected: <p>{filePath}</p>
-          </>
-        ) : (
-          "No Image Currently Selected"
-        )}
-      </h3>
-      <div>
-        <button onClick={handleOpen}>
-          {!imgPath ? "Open Image" : "Change Image"}
-        </button>
-
-        <button onClick={handleClose}>Remove Image</button>
+    <div className="container">
+      <div className="tooltip-bar">
+        <p className="header-text">
+          {filePath ? (
+            <>
+              <span>- Image path selected: {filePath} -</span>
+            </>
+          ) : (
+            "- No Image Loaded -"
+          )}
+        </p>
       </div>
+      <div className="editor">
+        <div className="button-panel">
+          <button onClick={handleOpen}>
+            {!imgPath ? "Open Image" : "Change Image"}
+          </button>
 
-      {imgPath && (
-        <div className="image-viewer">
-          <img src={imgPath} className="checkerboard-conic-background" />
+          <button onClick={handleClose}>Remove Image</button>
+          <span>Frame Count: {frames}</span>
+          <input
+            type="number"
+            name="Frame Count"
+            id="frame-count"
+            value={frames}
+            min={0}
+            onChange={(e) => {
+              const value = Math.max(0, Number(e.target.value));
+              setFrameCount(value);
+            }}
+          />
+          <span>
+            Image Width: <span>{innerWidth}</span>
+          </span>
+          <span>
+            Image Height: <span>{innerHeight}</span>
+          </span>
         </div>
-      )}
+
+        {imgPath && (
+          <div className="image-viewer">
+            <img src={imgPath} className="checkerboard-conic-background" />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
