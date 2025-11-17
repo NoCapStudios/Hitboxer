c8f20e561ffa176fdb34ae9f5c158ab92c8dc9bb
Dihyah Adib <dihyah.adib@gmail.com>
2025-11-16 19:14:41 -0500

    optimized rendering logic.

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -17,13 +17,8 @@ function App() {
   const [frames, setFrameCount] = useState<number>(1);
   const [scale, setScaleSize] = useState<number>(3);
   const [bgsize, setBgSize] = useState<number>(64);
-  const [imgSize, setImgSize] = useState({ width: 0, height: 0 });
 
-  useEffect(() => {
-    if (imgFlippedHorizontally) {
-    } else {
-    }
-  }, [imgFlippedHorizontally]);
+  const [imgSize, setImgSize] = useState({ width: 0, height: 0 });
 
   async function handleOpen() {
     const newFilePath = await window.electronAPI.openImageDialog();
@@ -49,73 +44,43 @@ function App() {
     setImgSize({ width: naturalWidth, height: naturalHeight });
   }
 
+  useEffect(() => {
+    if (imgFlippedHorizontally) {
+    } else {
+    }
+  }, [imgFlippedHorizontally]);
+
   function handleColorCoding(scale: number) {
     const atMax = scale === 16;
-    const sameScales = imgSize.width === imgSize.width * scale!;
-    if (atMax || sameScales) {
-      if (atMax) {
-        return (
-          <>
-            <span>
-              Image Width: <b className="scale-max">{imgSize.width} px</b>
-            </span>
-            <span>
-              Image Height: <b className="scale-max">{imgSize.height} px</b>
-            </span>
-            <div className="divider"></div>
-            <span>
-              Scaled Image Width:{" "}
-              <b className="scale-max">{imgSize.width * scale!} px</b>
-            </span>
-            <span>
-              Scaled Image Height:{" "}
-              <b className="scale-max">{imgSize.height * scale!} px</b>
-            </span>
-          </>
-        );
-      }
-      if (sameScales) {
-        return (
-          <>
-            <span>
-              Image Width: <b className="same-scales">{imgSize.width} px</b>
-            </span>
-            <span>
-              Image Height: <b className="same-scales">{imgSize.height} px</b>
-            </span>
-            <div className="divider"></div>
-            <span>
-              Scaled Image Width:{" "}
-              <b className="same-scales">{imgSize.width * scale!} px</b>
-            </span>
-            <span>
-              Scaled Image Height:{" "}
-              <b className="same-scales">{imgSize.height * scale!} px</b>
-            </span>
-          </>
-        );
-      }
-    } else {
-      return (
-        <>
-          <span>
-            Image Width: <b className="base-color">{imgSize.width} px</b>
-          </span>
-          <span>
-            Image Height: <b className="base-color">{imgSize.height} px</b>
-          </span>
-          <div className="divider"></div>
-          <span>
-            Scaled Image Width:{" "}
-            <b className="base-color">{imgSize.width * scale!} px</b>
-          </span>
-          <span>
-            Scaled Image Height:{" "}
-            <b className="base-color">{imgSize.height * scale!} px</b>
-          </span>
-        </>
-      );
-    }
+    const sameScales = imgSize.width === imgSize.width * scale;
+
+    const className = atMax
+      ? "scale-max"
+      : sameScales
+      ? "same-scales"
+      : "base-color";
+
+    return (
+      <>
+        <span>
+          Image Width: <b className={className}>{imgSize.width} px</b>
+        </span>
+        <span>
+          Image Height: <b className={className}>{imgSize.height} px</b>
+        </span>
+
+        <div className="divider"></div>
+
+        <span>
+          Scaled Image Width:{" "}
+          <b className={className}>{imgSize.width * scale} px</b>
+        </span>
+        <span>
+          Scaled Image Height:{" "}
+          <b className={className}>{imgSize.height * scale} px</b>
+        </span>
+      </>
+    );
   }
 
   return (
@@ -131,6 +96,7 @@ function App() {
           )}
         </p>
       </div>
+
       <div className="editor">
         <div className="button-panel">
           <button onClick={handleOpen}>
@@ -149,119 +115,90 @@ function App() {
             <ImageOff color="white" size={"1rem"} /> <span>Remove Image</span>
           </button>
 
+          {/* Horizontal Flip */}
           <button onClick={() => setHorizontal(!imgFlippedHorizontally)}>
-            {imgFlippedHorizontally ? (
-              <>
-                <FlipHorizontal color="white" size={"1rem"} />
-                <span>Flip Horizontally</span>
-              </>
-            ) : (
-              <>
-                <FlipHorizontal color="white" size={"1rem"} />
-                <span>Flip Back</span>
-              </>
-            )}
+            <FlipHorizontal size="1rem" color="white" />
+            <span>
+              {imgFlippedHorizontally ? "Flip Horizontal" : "Unflip Horizontal"}
+            </span>
           </button>
 
+          {/* Vertical Flip */}
           <button onClick={() => setVertical(!imgFlippedVertically)}>
-            {imgFlippedVertically ? (
-              <>
-                <FlipVertical color="white" size={"1rem"} />
-                <span>Flip Vertically</span>
-              </>
-            ) : (
-              <>
-                <FlipVertical color="white" size={"1rem"} />
-                <span>Flip Back</span>
-              </>
-            )}
-          </button>
-          {frames === 1 ? (
-            <span className="span-style">
-              Frame Count: {frames}{" "}
-              <span className="tiptool-text">(Default)</span>
+            <FlipVertical size="1rem" color="white" />
+            <span>
+              {imgFlippedVertically ? "Flip Vertical" : "Unflip Vertical"}
             </span>
-          ) : (
-            <span className="span-style">Frame Count: {frames}</span>
-          )}
-          <p className="tiptool-text">
-            Frame(s) count cannot be less than one.
-          </p>
+          </button>
+
+          {/* Frame Count */}
+          <span className="span-style">
+            Frame Count: {frames}{" "}
+            {frames === 1 && <span className="tiptool-text">(Default)</span>}
+          </span>
+
           <input
             type="number"
-            name="Frame Count"
             className="input-styles"
             value={frames}
             min={1}
-            onChange={(e) => {
-              const value = Math.max(0, Number(e.target.value));
-              setFrameCount(value);
-            }}
+            onChange={(e) => setFrameCount(Math.max(1, Number(e.target.value)))}
           />
-          {scale === 3 ? (
-            <span>
-              Sprite sheet Scale: {scale}{" "}
-              <span className="tiptool-text">(Default)</span>
-            </span>
-          ) : (
-            <span>Sprite sheet Scale: {scale}</span>
-          )}
-          <p className="tiptool-text">
-            Scaling iamges cannot be less than one.
-          </p>
+
+          {/* Scale */}
+          <span>
+            Sprite-sheet Scale: {scale}{" "}
+            {scale === 3 && <span className="tiptool-text">(Default)</span>}
+          </span>
+
           <input
             type="number"
-            name="Image Scale"
             className="input-styles"
             value={scale}
             min={1}
             max={16}
-            onChange={(e) => {
-              const value = Math.max(0, Number(e.target.value));
-              setScaleSize(value);
-            }}
+            onChange={(e) => setScaleSize(Math.max(1, Number(e.target.value)))}
           />
 
-          {bgsize === 64 ? (
-            <span>
-              Background Grid Size: {bgsize}{" "}
-              <span className="tiptool-text">(Default)</span>
-            </span>
-          ) : (
-            <span>Background Grid Size: {bgsize}</span>
-          )}
+          {/* BG Size */}
+          <span>
+            Background Grid Size: {bgsize}{" "}
+            {bgsize === 64 && <span className="tiptool-text">(Default)</span>}
+          </span>
+
           <p className="tiptool-text">
             Pro tip: Keep the number A multiple of 16 for better ratio to
             scalling results
           </p>
+
           <input
             type="number"
-            name="BG Grid Size"
             className="input-styles"
-            value={bgsize}
             min={16}
             step={16}
             max={512}
-            onChange={(e) => {
-              const value = Math.max(0, Number(e.target.value));
-              setBgSize(value);
-            }}
+            value={bgsize}
+            onChange={(e) => setBgSize(Math.max(16, Number(e.target.value)))}
           />
+
           {handleColorCoding(scale!)}
         </div>
 
         {imgPath && (
-          <div className="image-viewer">
+          <div
+            className="image-viewer"
+            style={{ ["--square-size" as any]: `${bgsize}px` }}
+          >
             <img
               src={imgPath}
-              className={`checkerboard-conic-background ${
-                imgFlippedHorizontally ? "" : "flip-h"
-              } ${imgFlippedVertically ? "" : "flip-v"}`}
-              width={imgSize.width * scale!}
-              height={imgSize.height * scale!}
               onLoad={handleImageLoad}
+              width={imgSize.width * scale}
+              height={imgSize.height * scale}
               alt="loaded"
               draggable={false}
+              className={`checkerboard-conic-background flip-anim ${
+                imgFlippedHorizontally ? "" : "flip-h"
+              } ${imgFlippedVertically ? "" : "flip-v"}`}
             />
           </div>
         )}
